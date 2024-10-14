4ebcac7561f8e57d1d1aea66b53d099ad1ed810b
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-15 00:17:50 +0300

    Up

--- src/class01/java01_methods03.java
+++ src/class01/java01_methods03.java
@@ -5,7 +5,6 @@ import java.util.Scanner;
 public class java01_methods03 {
     public static void main(String[] args) {
         greeting("John", "Doe");
-
     }
 
     private static void greeting(String name, String surName) {
@@ -14,7 +13,12 @@ public class java01_methods03 {
         name = sc.nextLine();  // kullanıcıdan alınan değeri parametreye atıyoruz
         System.out.println("What is your surname?");
         surName = sc.nextLine();  // kullanıcıdan alınan değeri parametreye atıyoruz
-        System.out.println("Hello " + name.toLowerCase().charAt(0).toUpperCase() + " " + surName.toLowerCase().charAt(0).toUpperCase() + ".");
+
+        // İlk harf büyük, geri kalan harfler küçük olacak şekilde dönüştürme
+        String formattedName = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
+        String formattedSurName = surName.substring(0, 1).toUpperCase() + surName.substring(1).toLowerCase();
+
+        System.out.println("Hello " + formattedName + " " + formattedSurName + ".");
         sc.close();
     }
 }
