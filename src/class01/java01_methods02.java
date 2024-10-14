18a8cd372c40d4105719611faed25b14910ef7ae
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-14 23:36:01 +0300

    Up

--- src/class01/java01_methods02.java
+++ src/class01/java01_methods02.java
@@ -2,35 +2,11 @@ package class01;
 
 public class java01_methods02 {
     public static void main(String[] args) {
-        greet();
-
-        // Provide example parameters
-        int a = 5;
-        int b = 10;
-
-        calculate(a, b);
+     String name = "John";
+     greeting(name);
     }
 
-    // Data types for parameters should be specified
-    private static void calculate(int a, int b) {
-        // Addition operation
-        int sum = a + b;
-        System.out.println("Sum: " + sum);
-
-        // Multiplication operation
-        int product = a * b;
-        System.out.println("Product: " + product);
-
-        // Subtraction operation
-        int difference = a - b;
-        System.out.println("Difference: " + difference);
-
-        // Division operation
-        int quotient = a / b;
-        System.out.println("Quotient: " + quotient);
-    }
-
-    private static void greet() {
-        System.out.println("Hello");
+    private static void greeting(String name) {
+        System.out.println("Hello " + name);
     }
 }
