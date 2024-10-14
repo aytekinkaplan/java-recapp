ff0ad9b87c4fcc4865fd7fcb5bfb5e665832cdb2
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-15 00:17:06 +0300

    Up

--- src/class01/java01_methods02.java
+++ src/class01/java01_methods02.java
@@ -1,12 +1,18 @@
 package class01;
 
+import java.util.Scanner;
+
 public class java01_methods02 {
     public static void main(String[] args) {
-     String name = "John";
-     greeting(name);
+        String name = "";  // boş bir string ile tanımladık
+        greeting(name);
     }
 
     private static void greeting(String name) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("What is your name?");
+        name = sc.nextLine();  // kullanıcıdan alınan değeri parametreye atıyoruz
         System.out.println("Hello " + name);
+        sc.close();
     }
 }
