1395e3d3814aa852c4deb8a990efd7249e2e191c
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-15 00:33:42 +0300

    Up

--- src/class01/java01_methods04.java
+++ src/class01/java01_methods04.java
@@ -1,24 +1,27 @@
 package class01;
 
-import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
 
 public class java01_methods04 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         greeting("John", "Doe");
     }
 
-    private static void greeting(String name, String surName) {
-        Scanner sc = new Scanner(System.in);
-        System.out.println("What is your name?");
-        name = sc.nextLine();  // kullanıcıdan alınan değeri parametreye atıyoruz
-        System.out.println("What is your surname?");
-        surName = sc.nextLine();  // kullanıcıdan alınan değeri parametreye atıyoruz
+    private static void greeting(String name, String surName) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        // İlk harf büyük, geri kalan harfler küçük olacak şekilde dönüştürme
+        System.out.println("What is your name?");
+        name = reader.readLine();  // kullanıcıdan alınan değeri parametreye atıyoruz
+
+        System.out.println("What is your surname?");
+        surName = reader.readLine();  // kullanıcıdan alınan değeri parametreye atıyoruz
+
+        // İlk harfi büyük yapıp geri kalanını küçük yapmak
         String formattedName = name.substring(0, 1).toUpperCase() + name.substring(1).toLowerCase();
         String formattedSurName = surName.substring(0, 1).toUpperCase() + surName.substring(1).toLowerCase();
 
         System.out.println("Hello " + formattedName + " " + formattedSurName + ".");
-        sc.close();
     }
 }
