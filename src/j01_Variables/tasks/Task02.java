8b134fad203ec760ec66074d6ddf2091cc282aa8
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-20 02:59:41 +0300

    Up

---
+++ src/j01_Variables/tasks/Task02.java
@@ -0,0 +1,7 @@
+package j01_Variables.tasks;
+
+public class Task02 {
+    public static void main(String[] args) {
+        System.out.println("Aytekin Kaplan\nHeight: 1.67\nWeight: 65.5\nAge: 44\nHobbies: Gaming, Reading, Programming, Movie Watching");
+    }
+}
