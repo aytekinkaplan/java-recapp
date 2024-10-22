b7417474d1a7b8ebf96b6325f3024c3d7019ce15
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-23 00:06:33 +0300

    Up

--- src/j04_JavaOperators/tasks/_02_ArithmeticOperator_sum2.java
+++ src/j04_JavaOperators/tasks/_02_ArithmeticOperator_sum2.java
@@ -1,16 +1,20 @@
-package ch04_JavaOperators.tasks04;
+package j04_JavaOperators.tasks;
 
 public class _02_ArithmeticOperator_sum2 {
 
     public static void main(String[] args) {
 
-     /*   Verilen 4 tane int vardır.
-        Int'lerin toplamlarını yazdırın  */
+        /* Task:
+           Given 4 integers, print their sum.
+        */
 
+        // Declare and initialize the integers
+        int num1 = 10, num2 = 20, num3 = 30, num4 = 40;
 
-        //Kodu aşağıya yazınız.
+        // Calculate the sum
+        int sum = num1 + num2 + num3 + num4;
 
-
-
+        // Print the result with a message
+        System.out.println("The sum of the four integers is: " + sum);
     }
 }
