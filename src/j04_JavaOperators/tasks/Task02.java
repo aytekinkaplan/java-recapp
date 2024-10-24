f3d8e87348168b49b22ab87272595f82a08e3d61
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-24 14:02:00 +0300

    Up

--- src/j04_JavaOperators/tasks/Task02.java
+++ src/j04_JavaOperators/tasks/Task02.java
@@ -1,25 +1,32 @@
 package j04_JavaOperators.tasks;
 
 public class Task02 {
-	public static void main(String[] args) {
-		/*
-		 Write a program that prints multiplication table for 1.
-		 Do not use any loop.
-		    1x1=1
-		    1x2=2
-			1x3=3
-			1x4=4
-			1x5=5
-			1x6=6
-			1x7=7	
-			1x8=8
-			1x9=9
-			1x10=10
-		 */
-
-
-
-
-
-	}
+    public static void main(String[] args) {
+        /*
+         Write a program that prints multiplication table for 1.
+         Do not use any loop.
+            1x1=1
+            1x2=2
+            1x3=3
+            1x4=4
+            1x5=5
+            1x6=6
+            1x7=7    
+            1x8=8
+            1x9=9
+            1x10=10
+         */
+        
+        // Printing the multiplication table of 1
+        System.out.println("1x1 = " + 1*1);
+        System.out.println("1x2 = " + 1*2);
+        System.out.println("1x3 = " + 1*3);
+        System.out.println("1x4 = " + 1*4);
+        System.out.println("1x5 = " + 1*5);
+        System.out.println("1x6 = " + 1*6);
+        System.out.println("1x7 = " + 1*7);
+        System.out.println("1x8 = " + 1*8);
+        System.out.println("1x9 = " + 1*9);
+        System.out.println("1x10 = " + 1*10);
+    }
 }
