03658d2262b072cede4efe92344c82492dba816c
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-29 00:20:16 +0300

    Up

---
+++ src/j08_Loops/l02_WhileLoop/tasks/Task07.java
@@ -0,0 +1,9 @@
+package j08_Loops.l02_WhileLoop.tasks;
+
+public class Task07 {
+    public static void main(String[] args) {
+        // task-> girilen  5 sayının  en büyüğünü print den code create eediniz
+
+
+    }
+}
