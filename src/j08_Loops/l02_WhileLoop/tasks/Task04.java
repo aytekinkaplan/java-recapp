d7b50858a80a522eba1a03ffa1f8694ca5c8dc16
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-29 00:19:31 +0300

    Up

---
+++ src/j08_Loops/l02_WhileLoop/tasks/Task04.java
@@ -0,0 +1,18 @@
+package j08_Loops.l02_WhileLoop.tasks;
+
+public class Task04 {
+
+	public static void main(String[] args) {
+		/*
+		task->  3 basamakli 4 ve 6 ya tam bolunebilen tam sayilari buyukten kucuge ve kaç tane oldugunu print eden code create ediniz.
+
+		 */
+
+		
+		
+
+		
+			
+	}
+
+}
