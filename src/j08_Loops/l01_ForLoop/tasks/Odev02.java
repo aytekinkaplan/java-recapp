42268a874d2f58834087c95e8774a9f83bbd0e5c
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-29 20:07:48 +0300

    Up

--- src/j08_Loops/l01_ForLoop/tasks/Odev02.java
+++ src/j08_Loops/l01_ForLoop/tasks/Odev02.java
@@ -5,21 +5,21 @@ import java.util.Scanner;
 public class Odev02 {
 
     public static void main(String[] args) {
-
-        /*
-        0 ile 100 arasındaki hem 5'e, hem de 4'e bölünebilen  sayıları print eden coode create edinz.
-
-
-         */
-
-        //Kodu aşağıya yazınız.
         Scanner scanner = new Scanner(System.in);
+
         System.out.print("Enter a number between 0 and 100 (0 and 100 included): ");
+        int number = scanner.nextInt();
 
+        if (number < 0 || number > 100) {
+            System.out.println("Please enter a valid number between 0 and 100.");
+        } else {
+            System.out.println("Numbers between 0 and " + number + " divisible by both 4 and 5:");
 
-
-
-
-
+            for (int i = 0; i <= number; i++) {
+                if (i % 4 == 0 && i % 5 == 0) {
+                    System.out.println(i);
+                }
+            }
+        }
     }
 }
