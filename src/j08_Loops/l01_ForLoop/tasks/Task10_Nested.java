226f85bdd9b1b040beff6754e520a3d67d035e8c
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-30 00:56:04 +0300

    Up

--- src/j08_Loops/l01_ForLoop/tasks/Task10_Nested.java
+++ src/j08_Loops/l01_ForLoop/tasks/Task10_Nested.java
@@ -1,31 +1,30 @@
-package j08_Loops.l01_ForLoop.tasks;
-
 public class Task10_Nested {
 
     public static void main(String[] args) {
-         /*
+        /*
+        The task is to print the following pattern:
+
         A
         B B
         C C C
         D D D D
         E E E E E
         F F F F F F
-        şekli yazdırınız
         */
 
-        int harf=65;
-        for (int i = 0; i < 6 ; i++) {
+        int harf = 65;  // ASCII value of 'A' is 65
 
-            for (int j = 0; j <= i ; j++) {//yine icteki loop a i kadar calis dedik
-                System.out.print( (char) ( harf + i )+" ");// i yi j kadar yazdirdi ayni satira. hep i yi yazdik
+        // Outer loop for rows
+        for (int i = 0; i < 6; i++) {
 
-
+            // Inner loop for printing characters in each row
+            for (int j = 0; j <= i; j++) {
+                // Print the character corresponding to (harf + i)
+                System.out.print((char) (harf + i) + " ");
             }
+
+            // Move to the next line after each row
             System.out.println();
         }
-      
-
-
-
     }
 }
