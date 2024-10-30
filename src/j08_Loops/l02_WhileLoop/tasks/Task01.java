b0fec10dbb4af68e4b1a26bcbb98e64087039cc9
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-30 23:50:35 +0300

    Up

--- src/j08_Loops/l02_WhileLoop/tasks/Task01.java
+++ src/j08_Loops/l02_WhileLoop/tasks/Task01.java
@@ -1,19 +1,23 @@
 package j08_Loops.l02_WhileLoop.tasks;
 
+import java.util.Scanner;
+
 public class Task01 {
     public static void main(String[] args) {
 /*
 TaskSahaf.txt-> girilen sayıların toplamı 333 gectiğinde girilen sayı adedini ve toplamını prşnt eden code create ediniz.
  */
 
+        Scanner scanner = new Scanner(System.in);
         int sum = 0;
         int count = 0;
         while (sum <= 333) {
-            sum += 11;
+            int number = scanner.nextInt();
+            sum += number;
             count++;
-        }
-        System.out.println("Sum = " + sum);
-        System.out.println("Count = " + count);
 
+        }
+        System.out.println(count);
+        System.out.println(sum);
     }
 }
