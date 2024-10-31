21f9ae9df7ea6b5321cc4ac8e399082763e2322f
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-31 12:14:31 +0300

    Up

--- src/j08_Loops/l02_WhileLoop/tasks/Task08.java
+++ src/j08_Loops/l02_WhileLoop/tasks/Task08.java
@@ -4,40 +4,43 @@ import java.util.Scanner;
 
 public class Task08 {
     public static void main(String[] args) {
-        // Kullanicidan toplamak uzere pozitif sayilar isteyin,
-        // islemi bitirmek icin 0’a basmasini soyleyin.
-        //	Kullanici yanlislikla negative sayi girerse o sayiyi dikkate almayin
-        //	ve “Negatif sayi giremezsiniz” yazdirip basa donun
-        //	Kullanici 0’a bastiginda toplam kac pozitif sayi girdigini,
-        //	yanlislikla kac negative sayi girdigini
-        //	ve girdigi pozitif sayilarin toplaminin kac oldugunu yazdirin.
-
+        /*
+        Task: Ask the user for positive numbers to add to a sum.
+        Inform the user that entering 0 will end the process.
+        Ignore negative numbers and display a warning when one is entered.
+        Once the user enters 0, display:
+        - The number of positive values entered
+        - The count of negative entries (ignored in sum)
+        - The total sum of the positive values entered
+        */
 
         Scanner input = new Scanner(System.in);
         int sumOfPositives = 0;
         int countOfPositives = 0;
         int countOfNegatives = 0;
-        String output1 = "You have entered " + countOfPositives + " positive number(s).";
-        String output2 = "You have entered " + countOfNegatives + " negative number(s).";
-        String output3 = "Your total is " + sumOfPositives + ".";
 
         while (true) {
-            System.out.println("Enter positive number or 0 to exit.");
+            System.out.println("Enter a positive number or 0 to exit:");
             int number = input.nextInt();
+
             if (number > 0) {
+                // Increment positive number count and add to sum
                 countOfPositives++;
                 sumOfPositives += number;
             } else if (number < 0) {
+                // Increment negative number count and notify the user
                 countOfNegatives++;
-            } else if (number == 0) {
-                System.out.println(output1);
-                System.out.println(output2);
-                System.out.println(output3);
+                System.out.println("Negative numbers are not allowed.");
+            } else {
+                // If the user enters 0, exit the loop and display results
+                System.out.println("You have entered " + countOfPositives + " positive number(s).");
+                System.out.println("You have entered " + countOfNegatives + " negative number(s) (ignored).");
+                System.out.println("The sum of positive numbers is " + sumOfPositives + ".");
                 break;
             }
-
         }
 
-
+        // Close the scanner
+        input.close();
     }
 }
