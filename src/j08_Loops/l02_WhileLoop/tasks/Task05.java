d496a46f1bf8af5a8b1f48bde69cf17d0bd80ad7
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-31 09:19:21 +0300

    Up

--- src/j08_Loops/l02_WhileLoop/tasks/Task05.java
+++ src/j08_Loops/l02_WhileLoop/tasks/Task05.java
@@ -1,17 +1,39 @@
 package j08_Loops.l02_WhileLoop.tasks;
 
+import java.util.Scanner;
+
 public class Task05 {
 
 	public static void main(String[] args) {
-		/*
-		task-> girilen  isim ve soyismi için ilk isminin ilk harfinden soy isminin son harfine kadar olan butun harfleri
-		 ayni satirda aralarina bosluk birakarak print rden code create ediniz
+        /*
+        Task: Create code to print all characters from the first letter of the first name
+        to the last letter of the surname, separated by spaces.
+        */
 
-		 */
-		
+		Scanner scan = new Scanner(System.in);
 
+		// Prompt the user to enter their first name
+		System.out.println("Enter your name: ");
+		String name = scan.nextLine();
 
+		// Prompt the user to enter their surname
+		System.out.println("Enter your surname: ");
+		String surname = scan.nextLine();
 
+		// Concatenate the first name and surname with a space between them
+		String fullName = name + " " + surname;
+
+		// Initialize index 'i' to iterate over each character in 'fullName'
+		int i = 0;
+
+		// Loop through each character in the full name string
+		while (i < fullName.length()) {
+			// Print each character followed by a space
+			System.out.print(fullName.charAt(i) + " ");
+			i++;
+		}
+
+		// Close the scanner to free up resources
+		scan.close();
 	}
-
 }
