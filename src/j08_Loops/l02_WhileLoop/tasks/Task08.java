31e9cf373452670f0162c2bd17853f0d5f82b3e7
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-31 12:11:37 +0300

    Up

--- src/j08_Loops/l02_WhileLoop/tasks/Task08.java
+++ src/j08_Loops/l02_WhileLoop/tasks/Task08.java
@@ -1,5 +1,7 @@
 package j08_Loops.l02_WhileLoop.tasks;
 
+import java.util.Scanner;
+
 public class Task08 {
     public static void main(String[] args) {
         // Kullanicidan toplamak uzere pozitif sayilar isteyin,
@@ -11,6 +13,18 @@ public class Task08 {
         //	ve girdigi pozitif sayilarin toplaminin kac oldugunu yazdirin.
 
 
+        Scanner input = new Scanner(System.in);
+        int sumOfPositives = 0;
+        int countOfPositives = 0;
+        int countOfNegatives = 0;
+        String output1 = "You have entered " + countOfPositives + " positive number(s).";
+        String output2 = "You have entered " + countOfNegatives + " negative number(s).";
+        String output3 = "Your total is " + sumOfPositives + ".";
+
+        while (true) {
+            System.out.println("Enter positive number or 0 to exit.");
+
+        }
 
 
     }
