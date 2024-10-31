0c33c61f4907227bc43eb9e313efee1ba027f085
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-10-31 08:19:34 +0300

    Up

--- src/j08_Loops/l02_WhileLoop/tasks/Task03.java
+++ src/j08_Loops/l02_WhileLoop/tasks/Task03.java
@@ -1,20 +1,40 @@
 package j08_Loops.l02_WhileLoop.tasks;
 
+import java.util.Scanner;
+
 public class Task03 {
 
 	public static void main(String[] args) {
-		/*
+        /*
+        Task: Create code that prints the multiplication table for an entered integer.
 
-        task-> girilen  tamsayı için carpim tablosunu print eden code create ediniz.
+        Example input = 13
+        Output:
+        13x1=13
+        13x2=26
+        ...
+        13x10=130
+        */
 
-		 Ornek input = 13
-		 13x1=13 13x2=26  . . .. .  . 13x10=130
-		 */
+		// Initialize a Scanner object to read user input
+		Scanner scan = new Scanner(System.in);
 
+		// Prompt the user to enter a number
+		System.out.println("Enter a number");
 
+		// Read and store the integer input in the variable 'num'
+		int num = scan.nextInt();
 
+		// Initialize the counter variable 'i' to 1
+		int i = 1;
 
+		// Use a while loop to generate and print the multiplication table up to 10
+		while (i <= 10) {
+			// Print the multiplication result for the current 'i' value
+			System.out.println(num + "x" + i + "=" + (num * i));
 
+			// Increment 'i' by 1 to progress to the next multiplication
+			i++;
+		}
 	}
-
 }
