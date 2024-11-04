86ff686006ed3a5b0d0a0d9852de639d9bf83c0c
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-05 00:06:17 +0300

    Up

--- src/j13_ArrayList/tasks/_07_arraylist7.java
+++ src/j13_ArrayList/tasks/_07_arraylist7.java
@@ -1,29 +1,44 @@
 package j13_ArrayList.tasks;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class _07_arraylist7 {
 
     /*
-        common_values() isminde bir method oluşturun.
-        Parametre olarak 2 tane Integer ArrayList
-        Return tipi integer ArrayList olmalı
-         İki arraylist alın ve arraylist'teki tüm ortak değerleri döndürün
-        Örneğin;
-        ArrayList 1:    3 , 2 , 5 , 6
-        ArrayList 2:     5 , 8 , 9
-        return  5 olmalı
+        Create a method called common_values().
+        It should take two Integer ArrayLists as parameters.
+        The return type should be an Integer ArrayList.
+        The method should return all the common values from both ArrayLists.
 
-        Örneğin;
-        ArrayList 1: 8,7,9,6,7
-        ArrayList 2: 6,7,12,3,1
-        return 6 ve 7 olmalı
+        Example:
+        ArrayList 1: 3, 2, 5, 6
+        ArrayList 2: 5, 8, 9
+        return should be [5]
+
+        Example:
+        ArrayList 1: 8, 7, 9, 6, 7
+        ArrayList 2: 6, 7, 12, 3, 1
+        return should be [6, 7]
      */
 
     public static void main(String[] args) {
+        ArrayList<Integer> list1 = new ArrayList<>(List.of(3, 2, 5, 6));
+        ArrayList<Integer> list2 = new ArrayList<>(List.of(5, 8, 9));
 
-
-
-
+        ArrayList<Integer> commonValues = common_values(list1, list2);
+        System.out.println("Common values: " + commonValues);
     }
 
+    private static ArrayList<Integer> common_values(ArrayList<Integer> list1, ArrayList<Integer> list2) {
+        ArrayList<Integer> common = new ArrayList<>();
 
+        for (Integer num : list1) {
+            if (list2.contains(num) && !common.contains(num)) {
+                common.add(num);
+            }
+        }
+
+        return common;
+    }
 }
