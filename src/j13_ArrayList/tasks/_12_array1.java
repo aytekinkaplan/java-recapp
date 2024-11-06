6dfdfc3c047855c2eaffd8a5eb7b4e8ea85fd076
AYTEKİN KAPLAN <112782378+aytekinkaplan@users.noreply.github.com>
2024-11-06 21:42:04 +0300

    Up

--- src/j13_ArrayList/tasks/_12_array1.java
+++ src/j13_ArrayList/tasks/_12_array1.java
@@ -1,29 +1,41 @@
 package j13_ArrayList.tasks;
 
+import java.util.HashSet;
+
 public class _12_array1 {
 
     /*
-    dupicate() isminde bir method oluşturun.
-    Parametresi int Array olmalı
-    Return tipi boolean
-    Eğer Array içinde çiftleme(yineleme) var ise true dönmeli.
-    Eğer Array içinde çiftleme(yineleme) yok ise false dönmeli.
-    
-    Örnek1:
-    Input: [1,2,3,1]
-    Output: true
+        Create a method called duplicate().
+        The parameter should be an int array.
+        The return type should be boolean.
 
-    Örnek 2:
-    Input: [1,2,3,4]
-    Output: false
-         */
+        If there are duplicates in the array, the method should return true.
+        If there are no duplicates, the method should return false.
+
+        Example 1:
+        Input: [1, 2, 3, 1]
+        Output: true
+
+        Example 2:
+        Input: [1, 2, 3, 4]
+        Output: false
+    */
 
     public static void main(String[] args) {
+        int[] array1 = {1, 2, 3, 1};
+        int[] array2 = {1, 2, 3, 4};
 
-
-
-
+        System.out.println("Array1 has duplicates: " + duplicate(array1)); // Output: true
+        System.out.println("Array2 has duplicates: " + duplicate(array2)); // Output: false
     }
 
-
+    private static boolean duplicate(int[] arr) {
+        HashSet<Integer> set = new HashSet<>();
+        for (int num : arr) {
+            if (!set.add(num)) { // If add() returns false, a duplicate is found
+                return true;
+            }
+        }
+        return false;
+    }
 }
